74d865226cff752b3784cee82eeb59de57964d4f
kerloshany3 <kerlostablet@gmail.com>
2024-12-03 22:52:52 +0200

    shit

--- app/components/ArticleDetils.jsx
+++ app/components/ArticleDetils.jsx
@@ -4,10 +4,10 @@ import Image from "next/image"
 const ArticleDetils = ({articleDetils}) => {
     return (
         <div>
-            <div>
+            <div className=' p-5'>
 
 
-                <h2>the number of paage :{articleDetils.title}</h2>
+                <h2 className=' text-white font-Abrial text-5xl w-3/4'>{articleDetils.title}</h2>
                 <h2>the number of paage :{articleDetils.description}</h2>
                 <h2>the number of paage :{articleDetils.isLive ? "shit" : "fuck"}</h2>
                 <h2>the number of paage :{articleDetils.publishedAt}</h2>
