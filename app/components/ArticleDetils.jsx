0387be62dfd1b31c56c52ae4c53cbd93a7ae34fc
kerloshany3 <kerlostablet@gmail.com>
2024-12-03 23:23:47 +0200

    sad

--- app/components/ArticleDetils.jsx
+++ app/components/ArticleDetils.jsx
@@ -2,27 +2,30 @@ import React from 'react'
 import Image from "next/image"
 
 const ArticleDetils = ({ articleDetils }) => {
+
+   
+    
+
     return (
-        <div>
+        
             <div className=' flex p-5'>
 
 
-                <div>
+                <div className=' w-3/4'>
 
 
 
                     <h2 className=' text-white font-Abrial text-6xl w-fit'>{articleDetils.title}</h2>
                     <p className=' font-medium text-3xl py-4 text-white/80'>{articleDetils.description}</p>
-                    <h2>the number of paage :{articleDetils.isLive ? "shit" : "fuck"}</h2>
-                    <h2>the number of paage :{articleDetils.publishedAt}</h2>
 
+                    
                 </div>
 
-                <div>
-                    <img src={articleDetils?.image?.url} className=' rounded-xl shadow-2xl shadow-black/40' width={1920} height={1080} alt="image" />
+                <div className=''>
+                    <img src={articleDetils?.image?.url} className=' rounded-xl shadow-2xl shadow-black/40 ' width={1000} height={500} alt="image" />
 
                 </div>
-            </div>
+            
         </div>
     )
 }
